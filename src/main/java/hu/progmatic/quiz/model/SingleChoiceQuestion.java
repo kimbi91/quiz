94003df5f6637d6f82743c79c1134ff542ae20fb
kimbi91 <subscribefan@gmail.com>
2022-07-30 09:29:20 +0200

    update

---
+++ src/main/java/hu/progmatic/quiz/model/SingleChoiceQuestion.java
@@ -0,0 +1,86 @@
+package hu.progmatic.quiz.model;
+
+public class SingleChoiceQuestion {
+    private long id;
+
+    private String question;
+
+    private int answer;
+
+    private String optionA;
+
+    private String optionB;
+
+    private String optionC;
+
+    private String optionD;
+
+    public SingleChoiceQuestion() {
+    }
+
+    public SingleChoiceQuestion(long id, String question, int answer, String optionA, String optionB, String optionC, String optionD) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public void setOptionD(String optionD) {
+        this.optionD = optionD;
+    }
+}
