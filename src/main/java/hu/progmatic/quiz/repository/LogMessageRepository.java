d19b6bc12db8c2edb51788d21baa03a8fcab12a5
kimbi91 <subscribefan@gmail.com>
2022-08-03 19:49:30 +0200

    update

--- src/main/java/hu/progmatic/quiz/repository/LogMessageRepository.java
+++ src/main/java/hu/progmatic/quiz/repository/LogMessageRepository.java
@@ -4,6 +4,10 @@ import hu.progmatic.quiz.model.LogMessage;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LogMessageRepository extends CrudRepository<LogMessage, Long> {
+
+  //  List<LogMessage> findAllByOrderByMessage();
 }
